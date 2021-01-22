14f2e002bf406c7a1ce46a43ed0d26777d04268a
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-22 05:16:57 +0000

    creating variables array

--- k8s-master.tf
+++ k8s-master.tf
@@ -16,20 +16,27 @@ variable "vpc_security_group_ids" {
 variable "cpu_credits" {
   default = "standard"
 }
+variable "tag_Name" {
+  default = ["k8s-master-ne1a", "k8s-master-ne1c", "k8s-master-ne1d"]
+}
+variable "private_ip" {
+  default = ["10.16.82.10", "10.16.84.10", "10.16.85.10"]
+}
 
-resource "aws_instance" "k8s-master-ne1a" {
+resource "aws_instance" "k8s-masters" {
+  count = length( var.tag_Name )
   ami = var.ami
   instance_type = var.instance_type
+  tags = {
+    Name = element(var.tag_Name, count.index)
+  }
   availability_zone = "ap-northeast-1a"
   subnet_id = var.subnet_id
-  private_ip = "10.16.82.10"
+  private_ip = element(var.private_ip, count.index)
   ipv6_address_count = 1
   vpc_security_group_ids = [
     var.vpc_security_group_ids
   ]
-  tags = {
-    Name = "k8s-master-ne1a"
-  }
   credit_specification {
     cpu_credits = var.cpu_credits
   }
