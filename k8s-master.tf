9fb5fcad9b7a983ad0b0eca18635fa4590ceb446
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-22 05:38:19 +0000

    included everything on array

--- k8s-master.tf
+++ k8s-master.tf
@@ -4,33 +4,28 @@ provider "aws" {
 variable "ami" {
   default = "ami-005eef3e36f4158d2"
 }
-variable "instance_type" {
-  default = "t3.small"
-}
 variable "subnet_id" {
   default = "subnet-0b03e0e88fb61e406"
 }
 variable "vpc_security_group_ids" {
   default = "sg-095b074aaacd65d1c"
 }
-variable "cpu_credits" {
-  default = "standard"
-}
-variable "tag_Name" {
-  default = ["k8s-master-ne1a", "k8s-master-ne1c", "k8s-master-ne1d"]
+
+variable "region_postfix" {
+  default = ["1a", "1c", "1d"]
 }
 variable "private_ip" {
   default = ["10.16.82.10", "10.16.84.10", "10.16.85.10"]
 }
-
+  
 resource "aws_instance" "k8s-masters" {
-  count = length( var.tag_Name )
+  count = length( var.region_postfix )
   ami = var.ami
-  instance_type = var.instance_type
+  instance_type = "t3.small"
   tags = {
-    Name = element(var.tag_Name, count.index)
+    Name = "k8s-master-${format("%s", element(var.region_postfix, count.index))}"
   }
-  availability_zone = "ap-northeast-1a"
+  availability_zone = "ap-northeast-${format("%s", element(var.region_postfix, count.index))}"
   subnet_id = var.subnet_id
   private_ip = element(var.private_ip, count.index)
   ipv6_address_count = 1
@@ -38,6 +33,6 @@ resource "aws_instance" "k8s-masters" {
     var.vpc_security_group_ids
   ]
   credit_specification {
-    cpu_credits = var.cpu_credits
+    cpu_credits = "standard"
   }
 }
