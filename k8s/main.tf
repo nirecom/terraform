9a57f9a6d88b33196c99d2c15872c0b4f32fd715
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-23 22:59:59 +0000

    split into variables.tf

--- k8s/main.tf
+++ k8s/main.tf
@@ -1,25 +1,10 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
-variable "ami" {
-  default = "ami-0e9208d1f0edf1344"
-}
-variable "vpc_security_group_ids" {
-  default = "sg-095b074aaacd65d1c"
-}
-
-variable "region_postfix" {
-  default = ["1a", "1c", "1d"]
-}
-variable "subnet_id" {
-  default = ["subnet-0b03e0e88fb61e406", "subnet-07c65d25ce8dbaacf", "subnet-0a787f89954a2a193"]
-}
-variable "private_ip" {
-  default = ["10.16.82.10", "10.16.84.10", "10.16.85.10"]
-}
-  
+ 
 resource "aws_instance" "k8s-masters" {
-  count = length( var.region_postfix )
+#  count = length( var.region_postfix )
+  count = 1
   ami = var.ami
   instance_type = "t3.small"
   tags = {
---
+++ k8s/variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  default = "ami-0340ee87079eb2c75"
+}
+variable "vpc_security_group_ids" {
+  default = "sg-095b074aaacd65d1c"
+}
+
+variable "region_postfix" {
+  default = ["1a", "1c", "1d"]
+}
+variable "subnet_id" {
+  default = ["subnet-0b03e0e88fb61e406", "subnet-07c65d25ce8dbaacf", "subnet-0a787f89954a2a193"]
+}
+variable "private_ip" {
+  default = ["10.16.82.10", "10.16.84.10", "10.16.85.10"]
+}
