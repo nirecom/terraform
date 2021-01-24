c15915352a9db68f29f8ad6a2f9c86e9c3356001
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-24 10:15:42 +0000

    changed subnet of lab

--- env/lab/main.tf
+++ env/lab/main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 module "module_ec2_lab" {
   source = "../../modules/"
 
-  ami = "ami-07eebf72274f24cb4"
+  ami = "ami-0834bee8eb6dae7f0"
   availability_zone = "ap-northeast-1a"
   instance_type = "t3.micro"
-  private_ip = "10.16.82.50"
-  subnet_id = "subnet-006a5ecaa0eaa23ed"
+  private_ip = "10.16.50.50"
+  subnet_id = "subnet-0b03e0e88fb61e406"
   tags_name = "lab"
   vpc_security_group_ids = "sg-0d2b31bcdda20aebe"
 }
