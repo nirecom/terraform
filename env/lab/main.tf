ce947fbda067af75ba25dcddb4469cbaeebdf7ee
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-24 11:20:30 +0000

    fixed az

--- env/lab/main.tf
+++ env/lab/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "ap-northeast-1"
+#  region = "ap-northeast-1"
 }
 
 module "module_ec2_lab" {
   source = "../../modules/"
 
   ami = "ami-0834bee8eb6dae7f0"
-  availability_zone = "ap-northeast-1a"
+  availability_zone = "ap-northeast-1c"
   instance_type = "t3.micro"
-  private_ip = "10.16.50.50"
-  subnet_id = "subnet-0b03e0e88fb61e406"
+  private_ip = "10.16.84.50"
+  subnet_id = "subnet-07c65d25ce8dbaacf"
   tags_name = "lab"
   vpc_security_group_ids = "sg-0d2b31bcdda20aebe"
 }
